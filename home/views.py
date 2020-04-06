6577aeeed02b67ccdd000e8055c049bdbe441ab7
MesutCanpolat <mesutcanpolat06@icloud.com>
2020-04-06 07:39:27 +0300

    contact form bize ulaşın formu yapıldı

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
@@ -16,12 +17,27 @@ def hakkimizda(request):
     context = {'setting': setting}
     return render(request, 'hakkimizda.html', context)
 
+
 def referanslar(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting': setting}
     return render(request, 'referanslarimiz.html', context)
 
+
 def iletisim(request):
+    if request.method == 'POST':  # form post edilsiyse
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()  # model ile bağlantı kur
+            data.name = form.cleaned_data['name']  # formdan bilgiyi al
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.save()  # veritabanına kayet
+            messages.success(request, "Mesajınız başarı ile gönderilmiştir. Teşekkür Ederiz")
+            return HttpResponseRedirect('/iletisim')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}
     return render(request, 'iletisim.html', context)
